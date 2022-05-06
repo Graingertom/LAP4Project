acc29f2654a0b5165f84ffe5173f0e51c67d9fe3
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-05-06 14:29:46 +0100

    adding postgresDB and css for profile

--- Bullhorn/Bullhorn/settings.py
+++ Bullhorn/Bullhorn/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'Bullhorn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Bullhorn', 
+        'USER': 'postgres', 
+        'PASSWORD': 'password',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
