e03676bfb95d3e86eb291841abc73b7743814f1c
Yan Wong <kelvinwong26546@gmail.com>
2022-05-06 20:47:33 +0800

    User Creation/Login finished

--- Bullhorn/frontend/urls.py
+++ Bullhorn/frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index),
+    path('app', index, name="Bullhorn_App"),
+    path('', views.base, name="User_Auth"),
 ]
--- Bullhorn/frontend/views.py
+++ Bullhorn/frontend/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# add login require
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+def base(request):
+    return render(request, "frontend/base.html")
