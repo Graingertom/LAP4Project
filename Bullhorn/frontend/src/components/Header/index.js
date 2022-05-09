d7ea703de3d61b2a5dba719c810b12cd550aa5be
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-05-09 12:05:45 +0100

    small frontend changes

--- Bullhorn/frontend/src/components/Header/index.js
+++ Bullhorn/frontend/src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const goTo = useNavigate();
+
     return(
     <>
         <section className='header'>
             <a>+</a>
-            <h1>Bullhorn</h1>
+            <h1 onClick={() => {goTo('/')}}>Bullhorn</h1>
             <a>Settings</a>
         </section>
     </>
