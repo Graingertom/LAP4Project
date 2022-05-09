35ca529da65d481d55455eb3718675f194732b33
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-05-09 16:11:54 +0100

    adding audio

--- Bullhorn/frontend/src/components/index.js
+++ Bullhorn/frontend/src/components/index.js
@@ -1,4 +1,6 @@
+
 export { default as ProfileBlock } from './ProfileBlock';
 export { default as FollowButton } from './FollowButton';
 export { default as Header } from './Header';
 export { default as Hamburger } from './Hamburger';
+export { default  as AudioBar } from './AudioBar';
