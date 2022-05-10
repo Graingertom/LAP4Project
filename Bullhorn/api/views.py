8d5620c336e92b83f686eb828c5f5db70c9a440e
Yan Wong <kelvinwong26546@gmail.com>
2022-05-10 18:44:14 +0800

    naming convention

--- Bullhorn/api/views.py
+++ Bullhorn/api/views.py
@@ -12,7 +12,7 @@ class PostListView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['mainUser']
+    filterset_fields = ['main_user']
     
     
 
@@ -20,12 +20,12 @@ class UserProfileView(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['mainUser', 'displayName']
+    filterset_fields = ['main_user', 'display_name']
 
 class FriendsView(viewsets.ModelViewSet):
     queryset = Friends.objects.all()
     serializer_class = FriendsSerializer
-    filterset_fields = ['mainUser']
+    filterset_fields = ['main_user']
     
 
 
