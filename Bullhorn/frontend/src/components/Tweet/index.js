b46d1d9f5d7ba716449a749d337c903fa4dc69c7
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-05-11 11:21:41 +0100

    the feed

--- Bullhorn/frontend/src/components/Tweet/index.js
+++ Bullhorn/frontend/src/components/Tweet/index.js
@@ -4,10 +4,22 @@ import { useParams } from 'react-router-dom';
 import { getUsers } from "../../actions";
 import FollowButton from '../FollowButton';
 
-const Tweet = () => {
+const Tweet = (props) => {
+	const url = "profile/" + props.main_user;
 	return (
 		<>
-			<h1>Yo</h1>
+			<div class="tweetBody">
+				<div class="tweetProfileNameArea">
+					<img src={props.pfp} class="tweetProfilePicture" />
+					<a class="tweetUrl" href={url}><p class="tweetAuthor">{props.main_user}</p></a>
+				</div>
+				<div class="tweetTitleAudioArea">
+					<h1 class="tweetTitle">{props.title}</h1>
+					<audio class="tweetAudio" controls>
+					<source src={props.audio} type="audio/mp3" />
+					</audio>
+				</div>
+			</div>
 		</>
 	)
 }
