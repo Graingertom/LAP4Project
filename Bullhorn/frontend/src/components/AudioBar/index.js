42515fc76d42519031ce764d875e137c0e7b2aa6
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-05-11 17:13:08 +0100

    trying to fix audio post

--- Bullhorn/frontend/src/components/AudioBar/index.js
+++ Bullhorn/frontend/src/components/AudioBar/index.js
@@ -5,6 +5,7 @@
 
 // visualiser setup - create web audio api context and canvas
 import React, { useState, useRef } from "react";
+import ConfirmationBox from "../ConfirmationBox";
 
 export default function AudioBar() {
   const [stream, setStream] = useState({
@@ -20,7 +21,10 @@ export default function AudioBar() {
   });
 
   const chunks = useRef([]);
-  const audioBlob = new Blob(chunks.current[0], { 'type' : 'audio/ogg; codecs=opus' })
+  const audioBlob = chunks.current[0]// , { 'type' : 'audio/ogg; codecs=opus' 
+  console.log(chunks.current[0]) 
+  
+  
 
   function getAccess() {
     navigator.mediaDevices
@@ -28,154 +32,12 @@ export default function AudioBar() {
       .then((mic) => {
         let mediaRecorder;
 
-<<<<<<< HEAD
         try {
           mediaRecorder = new MediaRecorder(mic, {
             mimeType: "audio/webm"
           });
         } catch (err) {
           console.log(err);
-=======
-  let onSuccess = function(stream) {
-    const mediaRecorder = new MediaRecorder(stream);
-
-    visualize(stream);
-
-    record.onclick = function() {
-      mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("recorder started");
-      record.style.background = "red";
-
-      stop.disabled = false;
-      record.disabled = true;
-    }
-
-    stop.onclick = function() {
-      mediaRecorder.stop();
-      console.log(mediaRecorder.state);
-      console.log("recorder stopped");
-      record.style.background = "";
-      record.style.color = "";
-      // mediaRecorder.requestData();
-
-      stop.disabled = true;
-      record.disabled = false;
-    }
-
-    mediaRecorder.onstop = function(e) {
-      console.log("data available after MediaRecorder.stop() called.");
-
-      // Upon successful creation create the clip post 
-      const clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
-
-      // Confirmation box 
-      const ConTitle = document.createElement('h1');
-      const ConContainer = document.createElement('article');
-      const ConClipLabel = document.createElement('p');
-      const ConAudio = document.createElement('audio');
-      const Confirmationbutton = document.createElement('button')
-      const ConDeleteButton = document.createElement('button');
-
-      // Post Audio
-      const clipContainer = document.createElement('article');
-      clipContainer.style.display = 'none'
-      const clipLabel = document.createElement('p');
-      const audio = document.createElement('audio');
-      const deleteButton = document.createElement('button');
-      
-      // Post config
-      ConTitle.classList.add('LastCheck')
-      ConTitle.textContent = 'Any last minute changes'
-      ConContainer.classList.add('clip');
-      ConAudio.setAttribute('controls', ''); // allows volume changing and downloading
-      Confirmationbutton.textContent = 'Keep'
-      Confirmationbutton.className = 'Keep'
-      ConDeleteButton.textContent = 'Delete';
-      ConDeleteButton.className = 'delete';
-
-
-      clipContainer.classList.add('clip');
-      audio.setAttribute('controls', ''); // allows volume changing and downloading
-      deleteButton.textContent = 'Delete';
-      deleteButton.className = 'delete';
-
-
-      clipName === null ? clipLabel.textContent = 'My BullHorn clip': clipLabel.textContent = clipName;
-      clipName === null ? ConClipLabel.textContent = 'My BullHorn clip': ConClipLabel.textContent = clipName;
-
-      // if( clipName === null) {
-      //   clipLabel.textContent = 'My BullHorn clip';
-      //   ConClipLabel.textContent = 'My BullHorn clip';
-      // } else {
-      //   clipLabel.textContent = clipName;
-      //   ConClipLabel.textContent = clipName
-      // }
-
-      // Confirmation box - confirm the post
-      ConContainer.appendChild(ConTitle);
-      ConContainer.appendChild(ConAudio);
-      ConContainer.appendChild(ConClipLabel);
-      ConContainer.appendChild(ConDeleteButton);
-      ConContainer.appendChild(Confirmationbutton);
-      console.log(ConContainer)
-      confirmationBox.appendChild(ConContainer) // confirmation box
-      
-      // Feed - Confirmed posts
-      clipContainer.appendChild(audio);
-      clipContainer.appendChild(clipLabel);
-      clipContainer.appendChild(deleteButton);
-      console.log(clipContainer)
-      soundClips.appendChild(clipContainer); // adding to the soundClips
-
-      // if (!!confirmationBox){
-      //   confirmationBox.parentNode.removeChild(clipContainer)
-      //   soundClips.appendChild(clipContainer)
-      // }
-
-      // Audio clip creation 
-      audio.controls = true;
-      ConAudio.controls = true;
-      const audioBlob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-      chunks = [];
-      const audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-      ConAudio.src = audioURL;
-      console.log("recorder stopped");
-
-      // Delete created post on feed
-      deleteButton.onclick = function(e) {
-        let evtTgt = e.target;
-        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-      }
-      Confirmationbutton.onclick = () => {
-        ConContainer.remove()
-        clipContainer.style.display = 'block'
-        // const audioCode = audioURL.split('/')
-        // console.log(audioCode[3])
-        const options = { 
-          method: 'POST',
-          body: {
-            'title': clipLabel,
-            'audio': audioBlob
-          },
-          headers: { "Content-Type": "application/json" }
-      };
-      console.log(options)
-  
-      fetch('http://localhost:5000/api/post', options)
-          .then(r => r.json())
-          .catch(console.warn)
-      }
-
-      clipLabel.onclick = function() {
-        const existingName = clipLabel.textContent;
-        const newClipName = prompt('Enter a new name for your sound clip?');
-        if(newClipName === null) {
-          clipLabel.textContent = existingName;
-        } else {
-          clipLabel.textContent = newClipName;
->>>>>>> 2560154c6c75e5b0cf9a5f937aaaea06d296de14
         }
 
         const track = mediaRecorder.stream.getTracks()[0];
@@ -192,11 +54,18 @@ export default function AudioBar() {
         mediaRecorder.ondataavailable = function (e) {
           console.log("data available");
           chunks.current.push(e.data);
+          
         };
 
         mediaRecorder.onstop = async function () {
           console.log("stopped");
-
+            const test = await chunks.current[0].arrayBuffer()
+            console.log("This is chunks "+  test)
+            console.log('This is audio Blob '+ audioBlob)
+            const formData = new FormData();
+            formData.append('audio', test)
+            console.log(formData)
+            
           const url = URL.createObjectURL(chunks.current[0]);
           chunks.current = [];
 
@@ -220,6 +89,7 @@ export default function AudioBar() {
   }
 
   return (
+      <>
     <div className="AudioBar">
       {stream.access ? (
         <div className="audio-container">
@@ -230,12 +100,15 @@ export default function AudioBar() {
             Start Recording
           </button>
           <button onClick={() => stream.recorder.stop()}>Stop Recording</button>
-          {recording.available && <audio controls src={recording.url} />}
+          {recording.available && <ConfirmationBox  audioURL={recording.url} /> }
         </div>
       ) : (
         <button onClick={getAccess}>Get Mic Access</button>
       )}
     </div>
+    
+    
+    </>
 
   );
 }
