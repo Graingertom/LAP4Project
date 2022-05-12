c4cae922bf4fd9a4bae923ff6e4ea984b274b777
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-05-12 14:37:46 +0100

    settings

--- Bullhorn/Bullhorn/settings.py
+++ Bullhorn/Bullhorn/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_URL = '../frontend/static'
+STATIC_URL = '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
