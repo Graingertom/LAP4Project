92e995475fc99830674f5e18d207c03e7a56b4b3
Tom Grainger <96476596+Graingertom@users.noreply.github.com>
2022-05-12 14:06:59 +0100

    settings

--- Bullhorn/Bullhorn/settings.py
+++ Bullhorn/Bullhorn/settings.py
@@ -151,8 +151,12 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_URL = 'static/'
+STATIC_URL = '../frontend/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
