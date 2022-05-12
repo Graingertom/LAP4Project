4cd1950aee61cfb435b57fd61a7e7660140e25d9
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-05-12 10:05:51 +0100

    class -> classname

--- Bullhorn/frontend/src/components/Tweet/index.js
+++ Bullhorn/frontend/src/components/Tweet/index.js
@@ -8,14 +8,14 @@ const Tweet = (props) => {
 	const url = "profile/" + props.main_user;
 	return (
 		<>
-			<div class="tweetBody">
-				<div class="tweetProfileNameArea">
-					<img src={props.pfp} class="tweetProfilePicture" />
-					<a class="tweetUrl" href={url}><p class="tweetAuthor">{props.main_user}</p></a>
+			<div className="tweetBody">
+				<div className="tweetProfileNameArea">
+					<img src={props.pfp} className="tweetProfilePicture" />
+					<a className="tweetUrl" href={url}><p className="tweetAuthor">{props.main_user}</p></a>
 				</div>
-				<div class="tweetTitleAudioArea">
-					<h1 class="tweetTitle">{props.title}</h1>
-					<audio class="tweetAudio" controls>
+				<div className="tweetTitleAudioArea">
+					<h1 className="tweetTitle">{props.title}</h1>
+					<audio className="tweetAudio" controls>
 					<source src={props.audio} type="audio/mp3" />
 					</audio>
 				</div>
